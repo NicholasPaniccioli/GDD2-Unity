bb1c01dfe5bdb4588528dd776c5ad3a6e7b630de
Kyle Mulvey <kfm4772@rit.edu>
2019-10-11 09:52:01 -0400

    Did the Quit script and started the SprigMove script.

--- Spriggans Save Christmas/Assets/Scripting/SprigMove.cs
+++ Spriggans Save Christmas/Assets/Scripting/SprigMove.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Started by Kyle Mulvey
+// Beginning to implement the generation of the grid.
+
 public class SprigMove : MonoBehaviour
 {
+    int x = 0;  // x coordinate
+    int y = 0;  // y coordinate
+    int xSize = 10; // number of squares in the x direction
+    int ySize = 10; // number of squares in the y direction
+
     // Start is called before the first frame update
     void Start()
     {
