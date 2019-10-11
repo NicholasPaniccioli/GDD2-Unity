d170a86514202f66ea9003db8a3350b568a632ea
sgm4689 <sgm4689@g.rit.edu>
2019-10-11 18:43:42 -0400

    Added HoldItem script

---
+++ Spriggans Save Christmas/Assets/Scripting/HoldItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldItem : MonoBehaviour
+{
+    List<GameObject> items;
+    int maxSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool GetItem(GameObject item)
+    {
+        if (items.Count < maxSize)
+        {
+            items.Add(item);
+            return true;
+        }
+        else
+            return false;
+    }
+}
