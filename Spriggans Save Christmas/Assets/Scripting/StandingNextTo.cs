0fbb0338b987fadeb9b0a3a9df8b754ae9fbaf6e
Christo Dallenbach <ctd2658@ad.rit.edu>
2019-10-11 19:24:34 -0400

    Did some movement and centered all the objects.

---
+++ Spriggans Save Christmas/Assets/Scripting/StandingNextTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingNextTo : MonoBehaviour
+{
+    Vector2 currentPos;
+    Vector2 forwardVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos = gameObject.transform.position;
+
+    }
+}
