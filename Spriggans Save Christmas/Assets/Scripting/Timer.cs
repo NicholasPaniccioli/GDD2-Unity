39f87ed388f0ea2f1f766b82658e83649a5418cd
sgm4689 <sgm4689@g.rit.edu>
2019-10-11 20:00:28 -0400

    created timer

---
+++ Spriggans Save Christmas/Assets/Scripting/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int timeLeft = 120;
+    public Text countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start thread for Timer
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown.text = ("" + timeLeft);
+    }
+
+    //Method to adjust the timer
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
