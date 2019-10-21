39f4de618e18944e7c0ae3296a449a98de7f5733
kfm4772 <kfm4772@rit.edu>
2019-10-20 20:51:20 -0400

    Made a Tile base class that Workbench, Refiner, Santa's Sack, Player, and Resource Piles will derive from. Did some of the logic for SantasSack.

---
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public string holdingName;  // name of the object the tile is holding
+    public bool isHolding;      // true if holding something, false if not holding something
+
+    public Tile()
+    {
+        isHolding = false;  // isHolding is false by default;
+    }
+
+    public Tile(bool _isHolding, string _holdingName)
+    {
+        isHolding = _isHolding; // the object is holding something or not
+        holdingName = _holdingName; // the name of the object that is held.
+    }
+}
+
---
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Workbench.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workbench : Tile
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Combine resources together to make a toy
+    void Craft()
+    {
+
+    }
+}
