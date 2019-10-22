b19e8b0add821b123eacfadc1e343c2cb2b38503
ctd2658 <ctd2658@rit.edu>
2019-10-22 17:47:28 -0400

    Added new script

---
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/HoldingBench.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingBench : Tile
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override bool Interact(Player player)
+    {
+        if (player.isHolding && !isHolding)
+        {
+            holdingName = player.holdingName;
+            holdingState = player.holdingState;
+            isHolding = true;
+            player.isHolding = false;
+            player.holdingName = "";
+            return true;
+        }
+        else if (isHolding && !player.isHolding)
+        {
+            player.isHolding = true;
+            player.holdingState = holdingState;
+            player.holdingName = holdingName;
+            isHolding = false;
+            holdingName = "";
+            return true;
+        }
+        else return false;
+    }
+}
