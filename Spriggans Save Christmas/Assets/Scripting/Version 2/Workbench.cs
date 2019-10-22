e460b7e7f15bae3fd9630efccee0da6bbf8c8fe6
Steven <sgm4689@IGM2025-25>
2019-10-22 17:14:56 -0400

    made workbench work

--- Spriggans Save Christmas/Assets/Scripting/Version 2/Workbench.cs
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Workbench.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Workbench : Tile//NOTE, workbench only works with 2 components.  If we decide we decide we want to change the amount of componenents a toy can take, this'll need to be redone.
 {
     // Start is called before the first frame update
+    public Tuple<string, HoldingState, string, HoldingState> dragonRecipe;
+    public Tuple<string, HoldingState, string, HoldingState> swordRecipe;
+    public Tuple<string, HoldingState, string, HoldingState> wandRecipe;
+    public Tuple<string, HoldingState, string, HoldingState> boatRecipe;
+
     void Start()
     {
-        
+        dragonRecipe = new Tuple<string, HoldingState, string, HoldingState>("Wood", HoldingState.state1, "Cloth", HoldingState.state2);
+        swordRecipe = new Tuple<string, HoldingState, string, HoldingState>("Wood", HoldingState.state3, "Wood", HoldingState.state1);
+        wandRecipe = new Tuple<string, HoldingState, string, HoldingState>("Wood", HoldingState.state3, "Cloth", HoldingState.state1);
+        boatRecipe = new Tuple<string, HoldingState, string, HoldingState>("Wood", HoldingState.state2, "Cloth", HoldingState.state3);
     }
 
     // Update is called once per frame
@@ -32,6 +41,84 @@ public class Workbench : Tile//NOTE, workbench only works with 2 components.  If
             if (player.isHolding)//if the player has something in hand, combine the 2 into a toy
             {
                 //if (compare the item in the players hand, plus the item already on the bench, to a list of possible combinations)
+                if (player.holdingState == HoldingState.unrefined || player.holdingState == HoldingState.finished || holdingState == HoldingState.unrefined || holdingState == HoldingState.finished)
+                {
+                    return false;//these items can't be combined into a toy, so return early
+                }
+                #region compareItems
+                    if (player.holdingName == dragonRecipe.Item1 && player.holdingState == dragonRecipe.Item2 && holdingName == dragonRecipe.Item3 && holdingState == dragonRecipe.Item4)
+                    {
+                        player.holdingName = "dragon";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (holdingName == dragonRecipe.Item1 && holdingState == dragonRecipe.Item2 && player.holdingName == dragonRecipe.Item3 && player.holdingState == dragonRecipe.Item4)
+                    {
+                        player.holdingName = "dragon";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (player.holdingName == swordRecipe.Item1 && player.holdingState == swordRecipe.Item2 && holdingName == swordRecipe.Item3 && holdingState == swordRecipe.Item4)
+                    {
+                        player.holdingName = "sword";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (holdingName == swordRecipe.Item1 && holdingState == swordRecipe.Item2 && player.holdingName == swordRecipe.Item3 && player.holdingState == swordRecipe.Item4)
+                    {
+                        player.holdingName = "sword";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (player.holdingName == wandRecipe.Item1 && player.holdingState == wandRecipe.Item2 && holdingName == wandRecipe.Item3 && holdingState == wandRecipe.Item4)
+                    {
+                        player.holdingName = "wand";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (holdingName == wandRecipe.Item1 && holdingState == wandRecipe.Item2 && player.holdingName == wandRecipe.Item3 && player.holdingState == wandRecipe.Item4)
+                    {
+                        player.holdingName = "wand";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (player.holdingName == boatRecipe.Item1 && player.holdingState == boatRecipe.Item2 && holdingName == boatRecipe.Item3 && holdingState == boatRecipe.Item4)
+                    {
+                        player.holdingName = "boat";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                    else if (holdingName == boatRecipe.Item1 && holdingState == boatRecipe.Item2 && player.holdingName == boatRecipe.Item3 && player.holdingState == boatRecipe.Item4)
+                    {
+                        player.holdingName = "boat";
+                        player.holdingState = HoldingState.finished;
+                        holdingState = HoldingState.unrefined;
+                        holdingName = "";
+                        isHolding = false;
+                        return true;
+                    }
+                #endregion
                 //if this is true, set the players holdingState to HoldingState.finished, their isHolding to true, and their holdingName to the completed toys name
                 //then set the bench's holdingName to "", and the isHolding to false
             }
