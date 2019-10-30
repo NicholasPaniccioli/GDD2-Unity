5a14489584b3d9fe9a065972d57872dd11269699
Nicholas Paniccioli <njp7745@g.rit.edu>
2019-10-30 09:50:12 -0400

    Finish the tutorial level nd the text with it.

--- Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public GameObject player; // Hold the player/spriggan
+    public int diaInd; //Number for indication and management of what text to be shown
+    public List <GameObject> dialogue; //Holds the text to be used
+    public GameObject coal; //Holds the coal in the toy list
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,44 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Goes to Coal-Text
+        if(player.transform.position.x > 3 && diaInd == 0)
+        {
+            dialogue[diaInd].SetActive(false);
+            diaInd++;
+            dialogue[diaInd].SetActive(true);
+        }
+
+        //Goes to Holding-Text
+        if(player.GetComponent<Player>().holdingName == "Coal" && diaInd == 1)
+        {
+            dialogue[diaInd].SetActive(false);
+            diaInd++;
+            dialogue[diaInd].SetActive(true);
+        }
+
+        //Goes to ToyList-Text
+        if(player.transform.position.x < 17 && diaInd == 2)
+        {
+            dialogue[diaInd].SetActive(false);
+            diaInd++;
+            dialogue[diaInd].SetActive(true);
+        }
+
+        //Goes to Santa-Text
+        if(player.transform.position.x < 12 && diaInd == 3)
+        {
+            dialogue[diaInd].SetActive(false);
+            diaInd++;
+            dialogue[diaInd].SetActive(true);
+        }
+
+        //Goes to Continue-Text
+        if (coal.active == false && diaInd == 4)
+        {
+            dialogue[diaInd].SetActive(false);
+            diaInd++;
+            dialogue[diaInd].SetActive(true);
+        }
     }
 }
