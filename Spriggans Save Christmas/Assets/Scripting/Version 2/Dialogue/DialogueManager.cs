03f2a95fcc5292dfe02680083acedbd02463f4a0
Nicholas <njp7745@g.rit.edu>
2019-10-31 13:21:54 -0400

    Finished the first level tutorial andf started the logic for dialogue on the second tutorial level.

--- Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class DialogueManager : MonoBehaviour
     public int diaInd; //Number for indication and management of what text to be shown
     public List <GameObject> dialogue; //Holds the text to be used
     public GameObject coal; //Holds the coal in the toy list
+    public GameObject sword; //Hold the sword in the toy list
 
 
     // Start is called before the first frame update
@@ -21,16 +23,18 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Goes to Coal-Text
-        if(player.transform.position.x > 3 && diaInd == 0)
+        //Goes to Coal-Text or the Station-Text
+        if(player.transform.position.x > 3 && diaInd == 0 || 
+            player.transform.position.x > 9 && player.transform.position.y > 3 && diaInd == 6)
         {
             dialogue[diaInd].SetActive(false);
             diaInd++;
             dialogue[diaInd].SetActive(true);
         }
 
-        //Goes to Holding-Text
-        if(player.GetComponent<Player>().holdingName == "Coal" && diaInd == 1)
+        //Goes to Holding-Text or the Sword-Text
+        if (player.GetComponent<Player>().holdingName == "Coal" && diaInd == 1 ||
+            Input.GetKeyDown(KeyCode.Space) && diaInd == 7)
         {
             dialogue[diaInd].SetActive(false);
             diaInd++;
@@ -38,7 +42,8 @@ public class DialogueManager : MonoBehaviour
         }
 
         //Goes to ToyList-Text
-        if(player.transform.position.x < 17 && diaInd == 2)
+        if(player.transform.position.x < 12 && diaInd == 2 || 
+            Input.GetKeyDown(KeyCode.Space) && diaInd == 8)
         {
             dialogue[diaInd].SetActive(false);
             diaInd++;
@@ -46,7 +51,7 @@ public class DialogueManager : MonoBehaviour
         }
 
         //Goes to Santa-Text
-        if(player.transform.position.x < 12 && diaInd == 3)
+        if(player.transform.position.y > 3 && diaInd == 3)
         {
             dialogue[diaInd].SetActive(false);
             diaInd++;
@@ -59,6 +64,17 @@ public class DialogueManager : MonoBehaviour
             dialogue[diaInd].SetActive(false);
             diaInd++;
             dialogue[diaInd].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && diaInd == 5)
+        {
+           diaInd++;
+            SceneManager.LoadScene("SecondLevel");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && diaInd == 11)
+        {
+            SceneManager.LoadScene("TheCOolOurScene");
         }
     }
 }
