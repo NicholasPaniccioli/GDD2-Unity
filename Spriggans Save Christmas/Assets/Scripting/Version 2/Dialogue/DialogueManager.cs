75ee8eda40a5bafce409a9d52939ee7097d67d86
Nicholas <njp7745@g.rit.edu>
2019-10-31 22:52:21 -0400

    Finished the second Tutorial and the dialogue for it.

--- Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Dialogue/DialogueManager.cs
@@ -11,7 +11,9 @@ public class DialogueManager : MonoBehaviour
     public int diaInd; //Number for indication and management of what text to be shown
     public List <GameObject> dialogue; //Holds the text to be used
     public GameObject coal; //Holds the coal in the toy list
+    public GameObject wood; //Holds the wood pile for reference
     public GameObject sword; //Hold the sword in the toy list
+    public GameObject refiner; //Holds the refiner for reference
 
 
     // Start is called before the first frame update
@@ -27,44 +29,125 @@ public class DialogueManager : MonoBehaviour
         if(player.transform.position.x > 3 && diaInd == 0 || 
             player.transform.position.x > 9 && player.transform.position.y > 3 && diaInd == 6)
         {
-            dialogue[diaInd].SetActive(false);
-            diaInd++;
-            dialogue[diaInd].SetActive(true);
+            if(diaInd == 0)
+            {
+                dialogue[diaInd].SetActive(false);
+                diaInd++;
+                dialogue[diaInd].SetActive(true);
+            }
+            else if (diaInd == 6)
+            {
+                dialogue[diaInd - 6].SetActive(false);
+                diaInd++;
+                dialogue[diaInd - 6].SetActive(true);
+            }
         }
 
         //Goes to Holding-Text or the Sword-Text
         if (player.GetComponent<Player>().holdingName == "Coal" && diaInd == 1 ||
             Input.GetKeyDown(KeyCode.Space) && diaInd == 7)
         {
-            dialogue[diaInd].SetActive(false);
-            diaInd++;
-            dialogue[diaInd].SetActive(true);
+            if (diaInd == 1)
+            {
+                dialogue[diaInd].SetActive(false);
+                diaInd++;
+                dialogue[diaInd].SetActive(true);
+            }
+            else if (diaInd == 7)
+            {
+                dialogue[diaInd - 6].SetActive(false);
+                diaInd++;
+                dialogue[diaInd - 5].SetActive(true);
+            }
         }
 
         //Goes to ToyList-Text
         if(player.transform.position.x < 12 && diaInd == 2 || 
-            Input.GetKeyDown(KeyCode.Space) && diaInd == 8)
+             diaInd == 8 && player.GetComponent<Player>().holdingName == "Wood")
         {
-            dialogue[diaInd].SetActive(false);
-            diaInd++;
-            dialogue[diaInd].SetActive(true);
+            if (diaInd == 2)
+            {
+                dialogue[diaInd].SetActive(false);
+                diaInd++;
+                dialogue[diaInd].SetActive(true);
+            }
+            else if (diaInd == 8)
+            {
+                dialogue[diaInd - 7].SetActive(false);
+                diaInd++;
+                dialogue[diaInd - 6].SetActive(true);
+                
+            }
         }
 
-        //Goes to Santa-Text
-        if(player.transform.position.y > 3 && diaInd == 3)
+        //Goes to Santa-Text or Refiner Text
+        if(player.transform.position.y > 3 && diaInd == 3
+            || refiner.GetComponent<Refiner>().holdingName == "Wood" && diaInd == 9)
         {
-            dialogue[diaInd].SetActive(false);
-            diaInd++;
-            dialogue[diaInd].SetActive(true);
+            if (diaInd == 3)
+            {
+                dialogue[diaInd].SetActive(false);
+                diaInd++;
+                dialogue[diaInd].SetActive(true);
+            }
+            else if (diaInd == 9)
+            {
+                dialogue[diaInd - 6].SetActive(false);
+                diaInd++;
+                dialogue[diaInd - 6].SetActive(true);
+            }
         }
 
-        //Goes to Continue-Text
-        if (coal.active == false && diaInd == 4)
+        //Goes to Bench-Text
+        if (coal.active == false && diaInd == 4
+            || Input.GetKeyDown(KeyCode.Space) && diaInd == 10)
         {
-            dialogue[diaInd].SetActive(false);
-            diaInd++;
-            dialogue[diaInd].SetActive(true);
+            if (diaInd == 4)
+            {
+                dialogue[diaInd].SetActive(false);
+                diaInd++;
+                dialogue[diaInd].SetActive(true);
+            }
+            else if (diaInd == 10)
+            {
+                dialogue[diaInd - 6].SetActive(false);
+                diaInd++;
+                dialogue[diaInd - 6].SetActive(true);
+            }
         }
+
+        //Goes to Craft-Text
+        if(Input.GetKeyDown(KeyCode.Space) && diaInd == 11 && (player.GetComponent<Player>().holdingName == "Wood"))
+        {
+            dialogue[diaInd - 6].SetActive(false);
+            diaInd++;
+            dialogue[diaInd - 6].SetActive(true);
+        }
+
+        //Goes to Finish Text
+        if (Input.GetKeyDown(KeyCode.Space) && diaInd == 12 && player.GetComponent<Player>().holdingName == "Sword")
+        {            
+            dialogue[diaInd - 6].SetActive(false);
+            diaInd++;
+            dialogue[diaInd - 6].SetActive(true);
+            
+        }
+
+        //Goes to Santa text
+        if(diaInd == 13 && player.transform.position.x < 12)
+        {         
+            dialogue[diaInd - 6].SetActive(false);
+            diaInd++;
+            dialogue[diaInd - 6].SetActive(true);           
+        }
+        //Goes to Continue text
+        if(sword.active == false && diaInd == 14)
+        {            
+            dialogue[diaInd - 6].SetActive(false);
+            diaInd++;
+            dialogue[diaInd - 6].SetActive(true);          
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space) && diaInd == 5)
         {
@@ -72,7 +155,7 @@ public class DialogueManager : MonoBehaviour
             SceneManager.LoadScene("SecondLevel");
         }
 
-        if(Input.GetKeyDown(KeyCode.Space) && diaInd == 11)
+        if(Input.GetKeyDown(KeyCode.N) && diaInd == 15)
         {
             SceneManager.LoadScene("TheCOolOurScene");
         }
