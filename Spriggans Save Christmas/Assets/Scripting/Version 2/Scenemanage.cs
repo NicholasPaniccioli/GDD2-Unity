d84b8e61ae3778fb659494d0458bdf974f4ff6c2
kfm4772 <kfm4772rit.edu>
2019-11-01 00:24:00 -0400

    Spruced up some of the UI so they all mostly have the same look. Added background music to the Start Screen, Tutorial levels, and Level 1.

--- Spriggans Save Christmas/Assets/Scripting/Version 2/Scenemanage.cs
+++ Spriggans Save Christmas/Assets/Scripting/Version 2/Scenemanage.cs
@@ -5,21 +5,40 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Scenemanage : MonoBehaviour {
+
+    string sceneName;
     //  Loads the main game scene
     public void LoadGame() {
 
         Time.timeScale = 1f;
         PauseMenu.paused = false;
-        SceneManager.LoadScene("FirstLevel");
+        SceneManager.LoadScene("FirstLevel");   // load the First Level scene
     }
 
     public void QuitGame() {
         Debug.Log("Game Closing!");
-        Application.Quit();
+        Application.Quit(); // close the game (only works when built)
     }
 
     public void Instructions()
     {
-        SceneManager.LoadScene("Pause");
+        SceneManager.LoadScene("Instructions"); // load the Instructions scene
+    }
+
+    private void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name; // get the name of the current scene
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))    // if the player presses the escape key
+        {
+            if(sceneName == "Instructions") // if the scene is the Instructions scene
+            {
+                Debug.Log("Loading Start Screen");  // output message the console
+                SceneManager.LoadScene("StartScreen");  // load the Start Screen scene
+            }
+        }
     }
 }
